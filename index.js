f3f45ec4999f652b1a34f557cba29daff9670cee
anonymousid24111 <anonymousid2411@gmail.com>
2020-06-07 11:07:39 +0700

    modi

--- index.js
+++ index.js
@@ -5,24 +5,42 @@ var Mongoose = require('mongoose');
 Mongoose.connect("mongodb://localhost/project2",{ 
   useUnifiedTopology: true,
   useNewUrlParser: true    });
-var controller = require('./controllers/product.controller');
+  var cookieParser = require('cookie-parser')
 
+// var controller = require('./controllers/product.controller');
+var jwt = require('jsonwebtoken')
 var port = 3000;
 var app = express();
+app.use(cookieParser())
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-
-app.get('/studentlist', controller.index);
-app.get('/student/:id/edit', controller.getid);
-app.delete('/student/:id', controller.deleteid);
-app.put('/student/:id/edit', controller.edit);
-app.post('/student/new', controller.create);
-app.post('/login', (req, res)=>{
+app.all('/login',(req, res)=>{
   console.log(req.body)
-  res.status(200).json({
-    data: "hihi"
+  var token = jwt.sign({username: req.body.username}, "key",{ expiresIn: 30})
+  res.status(200).send(token)
+})
+app.all('/private',(req, res)=>{
+  if (!req.cookies.username) res.status(400).send('khong co tokent')
+  var token = jwt.verify(req.cookies.username, 'key', function(err, decoded){
+    if (err) {
+      res.status(201).send(err.message)
+    }
+    else{
+      res.status(200).send('ok')
+    }
   })
 })
+// app.get('/studentlist', controller.index);
+// app.get('/student/:id/edit', controller.getid);
+// app.delete('/student/:id', controller.deleteid);
+// app.put('/student/:id/edit', controller.edit);
+// app.post('/student/new', controller.create);
+// app.post('/login', (req, res)=>{
+//   console.log(req.body)
+//   res.status(200).json({
+//     data: "hihi"
+//   })
+// })
 // app.delete('/student/:id', controller.d)
 
 app.listen(port, function() {
